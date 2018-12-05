9a24216b67f71e8f232df56968606fae096925d9
greatabel <myreceiver2for2github@gmail.com>
2018-12-05 21:37:22 +0800

    只写一部分类型，剩下的部分让编译器去推导

--- i01type_infer.rs
+++ i01type_infer.rs
@@ -4,4 +4,19 @@ fn main() {
     vec.push(elem);
 
     println!("{:?}", vec);
+
+    // 只写一部分类型，剩下的部分让编译器去推导
+    let player_scores = [
+        ("Jack", 20), ("Jane", 23), ("Jill", 18), ("John", 19),
+    ];
+    let players: Vec<_> = player_scores
+        .iter()
+        .map(|&(player, _score)|{
+            player
+        })
+        .collect();
+
+    println!("{:?}", players);
+
+
 }
