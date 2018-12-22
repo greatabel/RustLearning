cfdd8e19c8acf3bebbfe8bb49a77c36cb130eda4
greatabel <myreceiver2for2github@gmail.com>
2018-12-22 22:39:21 +0800

    extend by trait

--- i10static.rs
+++ i10static.rs
@@ -5,8 +5,24 @@ impl T {
         println!{"value {}", this.0};
     }
 }
+
+
+trait Double {
+    fn double(&self) -> Self;
+}
+
+impl Double for i32 {
+    fn double(&self) -> i32 { *self * 2 }
+}
+
+
 fn main() {
     let x = T(42);
     // x.func(); 小数点方式调用是不合法的
     T::func(&x);
+
+    // 可以像成员方法一样调用
+    let x1 : i32 = 10.double();
+    println!("{}", x1);
+
 }
