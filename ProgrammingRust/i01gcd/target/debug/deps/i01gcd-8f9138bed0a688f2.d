dd0029bef1e4d42986f70ede416ea20e7cafe476
greatabel <myreceiver2for2github@gmail.com>
2018-12-26 23:52:12 +0800

    rm useless target

--- ProgrammingRust/i01gcd/target/debug/deps/i01gcd-8f9138bed0a688f2.d
+++
@@ -1,5 +0,0 @@
-/Users/wanchang/Downloads/AbelProject/RustLearning/ProgrammingRust/i01gcd/target/debug/deps/i01gcd-8f9138bed0a688f2: src/main.rs
-
-/Users/wanchang/Downloads/AbelProject/RustLearning/ProgrammingRust/i01gcd/target/debug/deps/i01gcd-8f9138bed0a688f2.d: src/main.rs
-
-src/main.rs:
--- ProgrammingRust/i01gcd/target/debug/i01gcd.d
+++
@@ -1,1 +0,0 @@
-/Users/wanchang/Downloads/AbelProject/RustLearning/ProgrammingRust/i01gcd/target/debug/i01gcd.dSYM: /Users/wanchang/Downloads/AbelProject/RustLearning/ProgrammingRust/i01gcd/src/main.rs
